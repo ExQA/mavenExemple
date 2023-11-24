d546a77b8290bfc8bc754ed425be8e371839f31a
denis.svetlichnyi <denis.svetlichnyi@ringteam.com>
2023-11-24 11:08:14 +0200

    ДЗ 13. calc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the first number:");
-        double num1 = scanner.nextDouble();
+        try {
+            System.out.println("Enter the first number:");
+            double num1 = readDoubleInput(scanner);
 
-        System.out.println("Enter the second number:");
-        double num2 = scanner.nextDouble();
+            System.out.println("Enter the second number:");
+            double num2 = readDoubleInput(scanner);
 
-        System.out.println("Select the operation (+, -, x, /):");
-        String operation = scanner.next();
+            System.out.println("Enter the operation (+, -, x, /):");
+            String operation = readOperation(scanner);
 
-        double result = calculate(num1, num2, operation);
-        System.out.println("Result: " + result);
+            double result = calculate(num1, num2, operation);
+            System.out.println("Result: " + result);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
+    private static double readDoubleInput(Scanner scanner) {
+        while (true) {
+            try {
+                return scanner.nextDouble();
+            } catch (Exception e) {
+                System.out.println("Incorrect number. Enter again:");
+                scanner.next();
+            }
+        }
+    }
+
+    private static String readOperation(Scanner scanner) {
+        while (true) {
+            String operation = scanner.next();
+            if (operation.matches("[+\\-x/]")) {
+                return operation;
+            } else {
+                System.out.println("Incorrect operation. Enter again: +, -, x, /");
+            }
+        }
     }
 
     private static double calculate(double num1, double num2, String operation) {
@@ -31,13 +59,10 @@ public class Main {
                 if (num2 != 0) {
                     return num1 / num2;
                 } else {
-                    System.out.println("It is not possible to divide by zero.");
-                    System.exit(1);
+                    throw new ArithmeticException("It is not possible to divide by zero..");
                 }
             default:
-                System.out.println("Incorrect operation. Operations are supported: +, -, x, /");
-                System.exit(1);
-                return 0;
+                throw new IllegalArgumentException("Incorrect operation.");
         }
     }
 }
