12af4844022e8c5239028cec94f2f9ae8cff3f3e
denis.svetlichnyi <denis.svetlichnyi@ringteam.com>
2023-12-14 19:51:38 +0200

    ДЗ 16. Pet shelter

--- src/main/java/org/shelter/service/PetJsonSerializer.java
+++ src/main/java/org/shelter/service/PetJsonSerializer.java
@@ -8,19 +8,19 @@ import java.io.File;
 public class PetJsonSerializer {
 
     private final static ObjectMapper mapper = new ObjectMapper();
-    private final static String fileName = "src/main/resources/data.json";
+    private final static String file = "src/main/resources/shelterData.json";
 
     public static PetShelterData readData() {
         try {
-            return mapper.readValue(new File(fileName), PetShelterData.class);
+            return mapper.readValue(new File(file), PetShelterData.class);
         } catch (Exception e) {
-            throw new RuntimeException("Error reading file " + fileName);
+            throw new RuntimeException("Error reading file " + file);
         }
     }
 
     public static boolean writeData(PetShelterData data) {
         try {
-            mapper.writeValue(new File(fileName), data);
+            mapper.writeValue(new File(file), data);
             return true;
         } catch (Exception e) {
             return false;
